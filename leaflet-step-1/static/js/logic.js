bfc32b9f61a96167b3930fb59c0beff2bb63bb89
Haley Huhtala <haley.huhtala@gmail.com>
2020-07-09 13:55:02 -0500

    added bubbles and legend

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,25 +1,39 @@
 
 // Creating map object
-var myMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 11
-  });
-  
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var map = L.map("map", {
+    center: [40.7128, -98.0059],
+    zoom: 3
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
+}).addTo(map);
 
-// Create Bubbles
-bubbles = L.bubbleLayer(ca_cities, { property: "population" })
-
+// console.log(chroma('red'));
 //API Call
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-d3.json(url, function(response){
-    console.log(response.features);
+d3.json(url, function (response) {
+    // console.log(response.features[0].properties.mag);
+
+    // let earthquakes = response.features
+    // console.log(earthquakes);
+    // earthquakes.forEach(function (f) {
+    //     console.log(f.properties.mag)
+    //     let mag = f.properties.mag
+    //     console.log(f.geometry.coordinates)
+    // });
+    // Create Bubbles
+    bubbles = L.bubbleLayer(response, { 
+    property: "mag", 
+    legend: true,
+    scale: chroma.brewer.Set1,
+    max_radius : 40,
+    tooltip : false })
+    bubbles.addTo(map);
 })
